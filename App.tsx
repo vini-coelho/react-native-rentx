34f8fba0111cdd00113c2919c24514cd72ba330c
Vinicius Coelho <vinicius_coelho@live.com>
2021-07-27 11:45:02 -0400

    feat: add Car details screen

--- App.tsx
+++ App.tsx
@@ -13,6 +13,7 @@ import {
 } from '@expo-google-fonts/archivo';
 
 import { Home } from './src/screens/Home';
+import { CarDetails } from './src/screens/CarDetails';
 
 import theme from './src/styles/theme';
 
@@ -31,7 +32,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <CarDetails />
     </ThemeProvider>
   );
 }
---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Accessory } from '../../components/Accessory';
+
+import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+
+import speedSvg from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import exchangeSvg from '../../assets/exchange.svg';
+import peopleSvg from '../../assets/people.svg';
+
+import {
+  CarImages,
+  Container,
+  Header,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories
+} from './styles';
+
+export function CarDetails() {
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+      <CarImages>
+        <ImageSlider
+          imagesUrl={['https://img2.gratispng.com/20180628/bea/kisspng-audi-rs5-car-audi-q5-audi-s5-motor-sport-5b359e505de2b8.8061450915302405923846.jpg']}
+        />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name='380 Km/h' icon={speedSvg} />
+          <Accessory name='3.2s' icon={accelerationSvg} />
+          <Accessory name='800 HP' icon={forceSvg} />
+          <Accessory name='Gasolina' icon={gasolineSvg} />
+          <Accessory name='Auto' icon={exchangeSvg} />
+          <Accessory name='2 pessoas' icon={peopleSvg} />
+        </Accessories>
+
+        <About>
+        Este é automóvel desportivo.
+        Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla.
+        É um belíssimo carro para quem gosta de acelerar.
+        </About>
+      </Content>
+    </Container>
+  );
+}
